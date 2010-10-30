b40469acf270192e1c680e7fc63858dc9f3b8a37
Robert Niederreiter <rnix@blackbox.(none)>
2010-10-30 10:37:39 +0200

    add root content template and include styles.css

--- src/bda/bfg/ugm/browser/__init__.py
+++ src/bda/bfg/ugm/browser/__init__.py
@@ -1,1 +1,16 @@
-#
+from repoze.bfg.view import static
+from bda.bfg.tile import registerTile
+from bda.bfg.app.browser.layout import ProtectedContentTile
+from bda.bfg.ugm.model.root import Root
+
+from bda.bfg.app import browser
+browser.ADDITIONAL_CSS.append('bda.bfg.ugm.static/styles.css')
+
+static_view = static('static')
+
+registerTile('content',
+             'bda.bfg.ugm:browser/templates/root.pt',
+             interface=Root,
+             class_=ProtectedContentTile,
+             permission='login',
+             strict=False)
