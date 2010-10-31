aa1295ea64d594041ff401c3715398849cffc38f
Robert Niederreiter <rnix@blackbox.(none)>
2010-10-31 16:20:12 +0100

    right col form stubs

--- src/bda/bfg/ugm/browser/groups.py
+++ src/bda/bfg/ugm/browser/groups.py
@@ -1,7 +1,9 @@
+from yafowil.base import factory
 from bda.bfg.tile import (
     tile,
     Tile,
 )
+from bda.bfg.app.browser.form import EditForm
 from bda.bfg.ugm.model.groups import Groups
 
 @tile('leftcolumn', 'templates/left_column.pt', interface=Groups,
@@ -14,3 +16,43 @@ class GroupsLeftColumn(Tile):
       permission='login')
 class GroupsRightColumn(Tile):
     pass
+
+@tile('editform', interface=Groups, permission="view")
+class GroupEditForm(EditForm):
+    """XXX: bind to group.
+    """
+    
+    @property
+    def form(self):
+        form = factory(u'form',
+                       name='editform',
+                       props={'action': self.nodeurl})
+        form['name'] = factory(
+            'field:error:label:text',
+            value = 'Gruppe 1',
+            props = {
+                'label': 'Name',
+            })
+        form['save'] = factory(
+            'submit',
+            props = {
+                'action': 'save',
+                'expression': True,
+                'handler': self.save,
+                'next': self.next,
+                'label': 'Save',
+            })
+        form['cancel'] = factory(
+            'submit',
+            props = {
+                'action': 'cancel',
+                'expression': True,
+                'handler': None,
+                'next': self.next,
+                'label': 'Cancel',
+                'skip': True,
+            })
+        return form
+    
+    def save(self, widget, data):
+        pass
--- src/bda/bfg/ugm/browser/users.py
+++ src/bda/bfg/ugm/browser/users.py
@@ -1,7 +1,9 @@
+from yafowil.base import factory
 from bda.bfg.tile import (
     tile,
     Tile,
 )
+from bda.bfg.app.browser.form import EditForm
 from bda.bfg.ugm.model.users import Users
 
 @tile('leftcolumn', 'templates/left_column.pt', interface=Users,
@@ -14,3 +16,55 @@ class UsersLeftColumn(Tile):
       permission='login')
 class UsersRightColumn(Tile):
     pass
+
+@tile('editform', interface=Users, permission="view")
+class UserEditForm(EditForm):
+    """XXX: bind to user.
+    """
+    
+    @property
+    def form(self):
+        form = factory(u'form',
+                       name='editform',
+                       props={'action': self.nodeurl})
+        form['name'] = factory(
+            'field:error:label:text',
+            value = 'Max Mustermann',
+            props = {
+                'label': 'Name',
+            })
+        form['email'] = factory(
+            'field:error:label:text',
+            value = 'foo@my-domain.com',
+            props = {
+                'label': 'E-Mail',
+            })
+        form['phone'] = factory(
+            'field:error:label:text',
+            value = '0815',
+            props = {
+                'label': 'Phone',
+            })
+        form['save'] = factory(
+            'submit',
+            props = {
+                'action': 'save',
+                'expression': True,
+                'handler': self.save,
+                'next': self.next,
+                'label': 'Save',
+            })
+        form['cancel'] = factory(
+            'submit',
+            props = {
+                'action': 'cancel',
+                'expression': True,
+                'handler': None,
+                'next': self.next,
+                'label': 'Cancel',
+                'skip': True,
+            })
+        return form
+    
+    def save(self, widget, data):
+        pass
