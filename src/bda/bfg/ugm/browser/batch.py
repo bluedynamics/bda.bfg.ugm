80339c85c3acbd6ccc1607a83eb43e5e9e3556ea
Robert Niederreiter <rnix@blackbox.(none)>
2010-10-31 12:53:16 +0100

    base styles for left column

--- src/bda/bfg/ugm/browser/batch.py
+++ src/bda/bfg/ugm/browser/batch.py
@@ -4,6 +4,8 @@ from bda.bfg.ugm.model.users import Users
 from bda.bfg.ugm.model.groups import Groups
 
 class ColumnBatch(Batch):
+    """Abstract UGM column batch.
+    """
     
     @property
     def display(self):
@@ -11,6 +13,7 @@ class ColumnBatch(Batch):
     
     @property
     def vocab(self):
+        # XXX: nothing senceful yet, dummy code for CSS
         ret = list()
         for char in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
                      'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
@@ -21,7 +24,35 @@ class ColumnBatch(Batch):
                 'visible': True,
                 'url': '',
             })
+        ret[3]['visible'] = False
+        ret[10]['current'] = True
         return ret
+    
+    # just want to dislpay the batch vocab, disable other UI elements.
+    
+    @property
+    def firstpage(self):
+        return None
+    
+    @property
+    def prevpage(self):
+        return None
+    
+    @property
+    def nextpage(self):
+        return None
+    
+    @property
+    def lastpage(self):
+        return None
+    
+    @property
+    def leftellipsis(self):
+        return ''
+
+    @property
+    def rightellipsis(self):
+        return ''
 
 @tile('columnbatch', interface=Users, permission='login')
 class UsersColumnBatch(ColumnBatch): pass
