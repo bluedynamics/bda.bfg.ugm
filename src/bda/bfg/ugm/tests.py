536c2d13a7d6453adacedb68ad729432f874ab7f
Florian Friesdorf <flo@chaoflow.net>
2010-11-10 00:11:24 +0100

    layered testing based on plone.testing/bda.ldap.testing

--- src/bda/bfg/ugm/tests.py
+++ src/bda/bfg/ugm/tests.py
@@ -1,30 +1,61 @@
-import doctest 
+# Copyright 2008-2010, BlueDynamics Alliance, Austria - http://bluedynamics.com
+# GNU General Public Licence Version 2 or later
+
+import doctest
+import interlude
+import pprint
 import unittest2 as unittest
-from pprint import pprint
-from interlude import interact
+
+from plone.testing import layered, Layer
+
+from bda.ldap.testing import LDIF_users300
+
+
+class Base(Layer):
+    """a stub for now
+    """
+
+BASE = Base()
+
+DOCFILES = [
+    ('model/ugm.txt', BASE),
+    ('model/setting.txt', BASE),
+    ('model/users.txt', BASE),
+    ('model/user.txt', LDIF_users300),
+    ('model/groups.txt', BASE),
+    ('model/group.txt', BASE),
+]
+
 
 optionflags = doctest.NORMALIZE_WHITESPACE | \
               doctest.ELLIPSIS | \
               doctest.REPORT_ONLY_FIRST_FAILURE
 
-TESTFILES = [
-    'model/ugm.txt',
-    'model/setting.txt',
-    'model/users.txt',
-    'model/user.txt',
-    'model/groups.txt',
-    'model/group.txt',
-]
+
+print """
+*******************************************************************************
+If testing while development fails, please check if memcached is installed and
+stop it if running.
+*******************************************************************************
+"""
 
 def test_suite():
-    return unittest.TestSuite([
-        doctest.DocFileSuite(
-            file, 
-            optionflags=optionflags,
-            globs={'interact': interact,
-                   'pprint': pprint},
-        ) for file in TESTFILES
-    ])
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(
+            doctest.DocFileSuite(
+                docfile,
+                globs={'interact': interlude.interact,
+                           'pprint': pprint.pprint,
+                           'pp': pprint.pprint,
+                           },
+                optionflags=optionflags,
+                ),
+            layer=layer,
+            )
+        for docfile, layer in DOCFILES
+        ])
+    return suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
